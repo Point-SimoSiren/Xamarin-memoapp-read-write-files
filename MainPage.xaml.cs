8e8778357625a19da431dbca45f18fa90c44b0d6
Point-SimoSiren <45048404+Point-SimoSiren@users.noreply.github.com>
2021-09-26 09:44:44 +0300

    Only these 2 files have changed after project initalization

---
+++ MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace XamarinTiedonTallennus
+{
+    public partial class MainPage : ContentPage
+    {
+        string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder
+            .LocalApplicationData), "muistio.txt");
+
+        string text = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            bool doesExist = File.Exists(fileName);
+
+            if (doesExist == true)
+            {
+                text = File.ReadAllText(fileName);
+                if (text.Length > 0)
+                {
+                    outputLabel.Text = text;
+                }
+                else
+                {
+                    outputLabel.Text = "Mitään ei ole talletettu muistioon.";
+                }
+                
+            }
+            else
+            {
+                outputLabel.Text = "Tervetuloa uudelle käyttäjälle!";
+            }
+        }
+
+        
+        private void tallennusNappi_Clicked(object sender, EventArgs e)
+        {
+            text = text + Environment.NewLine + inputKentta.Text;
+            File.WriteAllText(fileName, text);
+            text = File.ReadAllText(fileName);
+            outputLabel.Text = text;
+            inputKentta.Text = "";
+        }
+
+        private void poistoNappi_Clicked(object sender, EventArgs e)
+        {
+            poistoNappi.IsVisible = false;
+            vahvistusInfo.IsVisible = true;
+            vahvistusKytkin.IsVisible = true;
+        }
+
+        private void vahvistusKytkin_Toggled(object sender, ToggledEventArgs e)
+        {
+            File.WriteAllText(fileName, "");
+            text = "";
+            outputLabel.Text = "Muistiinpanot tyhjennetty.";
+            poistoNappi.IsVisible = true;
+            vahvistusInfo.IsVisible = false;
+            vahvistusKytkin.IsVisible = false;
+        }
+    }
+ }
+
